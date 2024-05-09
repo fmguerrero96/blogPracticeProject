a1701aee8dc34ec42d01ddef3595f568ba1ddc98
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-08 21:16:01 -0400

    Create 3 mock posts

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,22 @@ function App() {
           <a href=''>Register</a>
         </nav>
       </header>
+
+        <div className="post">
+          <img src="https://media.wired.com/photos/663a73d25701544accfd071e/master/w_2240,c_limit/Met-Gala-AI_AI-Generated.jpg" alt="" />
+          <h2>Met Gala Deepfakes Are Flooding Social Media</h2>
+          <p>The Met Gala is undoubtedly one of the most anticipated events of the year, but this time the music and entertainment celebrities who graced its red carpet had some competition for the public's attention</p>
+        </div>
+        <div className="post">
+          <img src="https://media.wired.com/photos/663a73d25701544accfd071e/master/w_2240,c_limit/Met-Gala-AI_AI-Generated.jpg" alt="" />
+          <h2>Met Gala Deepfakes Are Flooding Social Media</h2>
+          <p>The Met Gala is undoubtedly one of the most anticipated events of the year, but this time the music and entertainment celebrities who graced its red carpet had some competition for the public's attention</p>
+        </div>
+        <div className="post">
+          <img src="https://media.wired.com/photos/663a73d25701544accfd071e/master/w_2240,c_limit/Met-Gala-AI_AI-Generated.jpg" alt="" />
+          <h2>Met Gala Deepfakes Are Flooding Social Media</h2>
+          <p>The Met Gala is undoubtedly one of the most anticipated events of the year, but this time the music and entertainment celebrities who graced its red carpet had some competition for the public's attention</p>
+        </div>
     </main>
   )
 }
