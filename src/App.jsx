8268537d3f5a764eff76e4e67915ba6b5ff2e3a8
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-08 21:04:25 -0400

    Create header with logo and nav links

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>test</div>
+    <main>
+      <header>
+        <a href='' className='logo'>MyBlog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+    </main>
   )
 }
 
