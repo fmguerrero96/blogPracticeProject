53062e2fac7b55bc9cc26f4ac1c72484c8bae895
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-17 14:25:39 -0600

    Link of class 'logo' redirects to index page

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Header() {
     return(
         <header>
-            <Link href='' className='logo'>MyBlog</Link>
+            <Link to={'/'} className='logo'>MyBlog</Link>
             <nav>
                 <Link to={'/login'}>login</Link>
                 <Link to={'/register'}>Register</Link>
