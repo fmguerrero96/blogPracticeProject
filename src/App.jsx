22093be788d6877644f17e22c0f50a4b1b5c5b15
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-17 13:12:31 -0600

    Import Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Post from './post'
 
 function App() {
 
@@ -13,52 +14,7 @@ function App() {
         </nav>
       </header>
 
-        <div className="post">
-          <div className="image">
-            <img src="https://assets-global.website-files.com/614716f50b4f49202fdd0087/6229b0f7cf6b6325a4f9ac6d_shopify-review-shopify-ecommerce-platform%20(1).jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2>Shopify shares plunge 18% on weak guidance</h2>
-            <p className="info">
-              <a className='author' href="">John Doe</a>
-              <time datetime="2024-05-08 21:45">2024-05-08 21:45</time>
-            </p>
-            <p className='summary'>
-              Shopify shares tumbled 18% on Wednesday, shaving almost $20 billion off the company’s value, after the company gave revenue and profit guidance for the current quarter that spooked investors.
-            </p>
-          </div>
-        </div>
-
-        <div className="post">
-          <div className="image">
-            <img src="https://assets-global.website-files.com/614716f50b4f49202fdd0087/6229b0f7cf6b6325a4f9ac6d_shopify-review-shopify-ecommerce-platform%20(1).jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2>Shopify shares plunge 18% on weak guidance</h2>
-            <p className="info">
-              <a className='author' href="">John Doe</a>
-              <time datetime="2024-05-08 21:45">2024-05-08 21:45</time>
-            </p>
-            <p className='summary'>
-              Shopify shares tumbled 18% on Wednesday, shaving almost $20 billion off the company’s value, after the company gave revenue and profit guidance for the current quarter that spooked investors.
-            </p>
-          </div>
-        </div>
-        <div className="post">
-          <div className="image">
-            <img src="https://assets-global.website-files.com/614716f50b4f49202fdd0087/6229b0f7cf6b6325a4f9ac6d_shopify-review-shopify-ecommerce-platform%20(1).jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2>Shopify shares plunge 18% on weak guidance</h2>
-            <p className="info">
-              <a className='author' href="">John Doe</a>
-              <time datetime="2024-05-08 21:45">2024-05-08 21:45</time>
-            </p>
-            <p className='summary'>
-              Shopify shares tumbled 18% on Wednesday, shaving almost $20 billion off the company’s value, after the company gave revenue and profit guidance for the current quarter that spooked investors.
-            </p>
-          </div>
-        </div>
+      <Post/>
     </main>
   )
 }
