06799b4290853023b99eb86e1a9d33e3a6c27fd3
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-17 15:00:48 -0600

    Create login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,9 @@
 export default function LoginPage() {
     return(
-        <>Login Page</>
+        <form action="">
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+        </form>
     )
 }
