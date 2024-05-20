f9a1e29eb7d6c8e8358f1ae7bbb63578fa1b3fb0
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-20 14:41:16 -0600

    Add useEffect hook to fetch user info if logged in

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,34 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export default function Header() {
+    const [username, setUsername] = useState('')
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include'
+        }).then(response =>{
+            response.json().then(userInfo => {
+                setUsername(userInfo.username)
+            })
+        })
+    }, []);
+
     return(
         <header>
             <Link to={'/'} className='logo'>MyBlog</Link>
             <nav>
-                <Link to={'/login'}>login</Link>
-                <Link to={'/register'}>Register</Link>
+                {username && (
+                    <>
+                        <Link to={'/create'}>Create new post</Link>
+                        <a>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to={'/login'}>login</Link>
+                        <Link to={'/register'}>Register</Link>
+                    </>
+                )}
             </nav>
         </header>
     )
