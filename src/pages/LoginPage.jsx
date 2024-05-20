aeeedac644a3d7c9e7cd940c1b8c4e850b9862e1
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-20 14:01:53 -0600

    Create function to handle login logic and navigate to homepage if successful login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,34 @@
 import { useState } from "react"
+import { Navigate } from "react-router-dom";
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false)
 
+    const login = async function(e) {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password}),
+            credentials: 'include',
+        })
+        if(response.ok){
+            setRedirect(true)
+        } else {
+            alert('Wrong Credentials')
+        }
+    }
+
+    if(redirect){
+        return(
+            <Navigate to={'/'}></Navigate>
+        )
+    }
     return(
-        <form className="login">
+        <form className="login" onSubmit={login}>
             <h1>Login</h1>
             <input type="text" placeholder="username"
                 value={username}
