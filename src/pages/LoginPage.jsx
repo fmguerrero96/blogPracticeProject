aaa6972e25add8b0ef079dbcad0e96770426a26f
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-19 20:32:52 -0600

    Set values and onChange funciton for inputs

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
         <form className="login">
             <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text" placeholder="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)} />
+            <input type="password" placeholder="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)} />
             <button>Login</button>
         </form>
     )
