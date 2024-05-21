28752745008c554d9b12a559cc919aa9d8a77d51
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-21 15:26:45 -0600

    Move modules and formats outside component and createNewPost function is unfinished

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,29 +2,43 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
 
+const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+}
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+]
+
 export default function CreatePost(){
     const [content, setContent] = useState('');
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
 
-    const modules = {
-        toolbar: [
-          [{ 'header': [1, 2, false] }],
-          ['bold', 'italic', 'underline','strike', 'blockquote'],
-          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-          ['link', 'image'],
-          ['clean']
-        ],
-    }
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-    ]
+    const createNewPost = function(e){
+        const data = new FormData();
+        data.set('title', title)
+        data.set('summary', summary)
+        data.set('content', content)
+        data.set('file', )
+
+        e.preventDefault()
+        fetch('http://localhost:4000/post',
+            method: 'POST', 
+            body: 
+        )
+    };
     
     return(
-        <form>
+        <form onSubmit={createNewPost}>
             <input type="text" 
                 placeholder="Title"
                 value={title} 
