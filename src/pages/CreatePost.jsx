3da7b2dd3c40c6d1c5e1beffd6a0ae07c7ebeef1
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-21 13:14:17 -0600

    Use react-quill rich text editor

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,10 +1,15 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+
 export default function CreatePost(){
+    const [value, setValue] = useState('');
     return(
         <form>
             <input type="text" placeholder="Title" />
             <input type="text" placeholder="Summary" />
             <input type="file" />
-            <textarea name=" " id=""></textarea>
+            <ReactQuill theme="snow" value={value} onChange={setValue} />;
         </form>
     )
 }
