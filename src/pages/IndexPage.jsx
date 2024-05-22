8bc29ed27dcf3fe8fe121e91a5bddecffe1a3dd2
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-22 16:38:00 -0600

    Fetch posts from db

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import Post from "../Post";
 
 export default function IndexPage() {
+    useEffect(() => {
+        fetch('http://localhost:4000/post').then(response => {
+            response.json().then(posts => {
+                console.log(posts)
+            })
+        })
+    })
+
     return(
         <>
             <Post/>
