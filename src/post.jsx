2a46bb13a6317ea929ce5db58872bbe8c32b81d4
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-23 12:58:23 -0600

    Links point to /post/_id

--- src/post.jsx
+++ src/post.jsx
@@ -1,16 +1,16 @@
 import { format } from "date-fns"
 import { Link } from "react-router-dom"
 
-export default function Post ({title, cover, summary, createdAt, author}) {  
+export default function Post ({title, cover, summary, createdAt, author, _id}) {  
     return(
         <div className="post">
           <div className="image">
-            <Link to={'/post/id'}>
+            <Link to={`/post/${_id}`}>
               <img src={'http://localhost:4000/'+cover} alt="" />
             </Link>
           </div>
           <div className="texts">
-            <Link to={'/post/id'}>
+            <Link to={`/post/${_id}`}>
               <h2>{title}</h2>
             </Link>
             <p className="info">
