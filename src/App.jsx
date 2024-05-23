74c4112d42b444a7b5013200e3896f98c08d4111
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-23 13:00:49 -0600

    Create route for PostPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import CreatePost from './pages/CreatePost'
 import { UserContextProvider } from './UserContext'
+import PostPage from './pages/PostPage'
 
 function App() {
 
@@ -21,6 +22,8 @@ function App() {
           <Route path={'/register'} element={<RegisterPage/>} />
 
           <Route path={'/create'} element={<CreatePost/>} />
+
+          <Route path={'/post/:id'} element={<PostPage/>}/>
         </Route>
       </Routes>
     </UserContextProvider>
