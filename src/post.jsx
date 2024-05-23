918df6a9f6f18beb72a58bbaa14039356334e31e
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-23 12:49:28 -0600

    Wrap img and title inside Link element

--- src/post.jsx
+++ src/post.jsx
@@ -1,13 +1,18 @@
 import { format } from "date-fns"
+import { Link } from "react-router-dom"
 
 export default function Post ({title, cover, summary, createdAt, author}) {  
     return(
         <div className="post">
           <div className="image">
-            <img src={'http://localhost:4000/'+cover} alt="" />
+            <Link to={'/post/id'}>
+              <img src={'http://localhost:4000/'+cover} alt="" />
+            </Link>
           </div>
           <div className="texts">
-            <h2>{title}</h2>
+            <Link to={'/post/id'}>
+              <h2>{title}</h2>
+            </Link>
             <p className="info">
               <a className='author' href="">{author.username}</a>
               <time>{format(createdAt, 'MMM d, yyyy HH:mm') }</time>
